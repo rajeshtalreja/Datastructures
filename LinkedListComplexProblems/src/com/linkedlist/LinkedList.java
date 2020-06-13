586b9936c3a4f042adfec05b2faf65594ca17a8a
DELL <DELL@192.168.236.1>
2020-06-14 00:03:48 +0530

    Added sorted list element add method.

--- LinkedListComplexProblems/src/com/linkedlist/LinkedList.java
+++ LinkedListComplexProblems/src/com/linkedlist/LinkedList.java
@@ -76,4 +76,37 @@ class LinkedListNode<T> {
 		return data.toString() + " -> ";
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LinkedListNode<T> other = (LinkedListNode<T>) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		return true;
+	}
+	
+	
+
 }
--- LinkedListComplexProblems/src/com/linkedlist/LinkedListComplexProblems.java
+++ LinkedListComplexProblems/src/com/linkedlist/LinkedListComplexProblems.java
@@ -1,11 +1,103 @@
 package com.linkedlist;
 
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * Following problems are implemented in this class - 
+ * 1. Linked List has a cycle or not
+ * 2. Insert an element in a sorted list.
+ * 3. 
+ * @author DELL
+ *
+ */
 public class LinkedListComplexProblems {
 	
 	public static void main(String[] args) {
 		LinkedList<Integer> list = new LinkedList<Integer>();
-		list.createListFromArray(new Integer[]{5,10,15,20});
+		list.createListFromArray(new Integer[]{5});
+		list.printList();
+		System.out.println(isCyclic(list));
+		//Cyclic list
+		LinkedList<Integer> cyclicList = new LinkedList<>();
+		cyclicList.createListFromArray(new Integer[]{5,10,15,20,30});
+		LinkedListNode<Integer> temp = cyclicList.getHead();
+		LinkedListNode<Integer> tail = cyclicList.getHead().getNext().getNext().getNext().getNext();
+		tail.setNext(temp.getNext());
+		System.out.println(isCyclic(cyclicList));
+		
+		insertInSortedList(list, 18);
 		list.printList();
 	}
+	
+	/**
+	 * This will return whether the list is cyclic or not
+	 * @return
+	 */
+	public static boolean isCyclic(LinkedList<Integer> list){
+		/**
+		 * There are lot of ways to implement this
+		 * 1. Use HashSet to add the values if the Node(not value) exists then add method will return false 
+		 * and the cycle exists.
+		 * 
+		 * 2. Add one flag called visited in the new node structure and iterate if we find already visited node then there is a cycle
+		 * 3. The execellent approach is create two pointers slow and fast
+		 * slow will move one step and fast will move two steps if they meet then there is a cycle
+		 */
+		
+		//This is HashSet method
+		// The time complexity is O(n) and space complexity O(n)
+		
+		/* Set<LinkedListNode<Integer>> set = new HashSet<>();
+		 LinkedListNode<Integer> temp = list.getHead();
+		 while(temp != null){
+			 if(!set.add(temp)){
+				 return true;
+			 }
+			 temp = temp.getNext();
+		 }
+		 return false;
+		*/
+		
+		/**
+		 * This is slow and fast pointer.
+		 */
+		LinkedListNode<Integer> slow = list.getHead();
+		LinkedListNode<Integer> fast = list.getHead().getNext();
+		while(slow != null && fast != null && fast.getNext() != null){
+			slow = slow.getNext();
+			fast =  fast.getNext().getNext();
+			if(slow == fast){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Insert the node in the sorted list
+	 * @param list
+	 */
+	public static void insertInSortedList(LinkedList<Integer> list, Integer element){
+		if(list == null){
+			return;
+		}
+		LinkedListNode<Integer> temp = list.getHead();
+		LinkedListNode<Integer> prev  = null;
+		while(temp != null){
+			if(temp.getData() > element){
+				break;
+			}
+			prev = temp;
+			temp = temp.getNext();
+		}
+		LinkedListNode<Integer> t = new LinkedListNode<>(element);
+		prev.setNext(t);
+		t.setNext(temp);
+	}
+	
+	
+	
 
 }
