a317c4f766302ccaf25bb15c517d21bba02c561c
DELL <DELL@192.168.236.1>
2020-06-29 01:19:00 +0530

    Added problems definitions.

--- DynamicProgramming/src/com/dp/DPProblems.java
+++ DynamicProgramming/src/com/dp/DPProblems.java
@@ -2,7 +2,59 @@ package com.dp;
 
 
 /**
+ * 1. Longest Common Subsequence(LCS) - Length and print the LCS - Recursive and DP solution
  * 
+ * 2. Longest Increasing Subsequence (LIS)
+ * 
+ * 3. Longest Decreasing Subsequence (LDS)
+ * 
+ * 4. Longest Bitonic Subsequence
+ * 
+ * 5. Longest Palindromic Subsequence(LPS)
+ * 
+ * 6. Longest Repeated Subsequence
+ * 
+ * 7. Longest Common Substring
+ * 
+ * 8. Shortest Common Supersequence
+ * 
+ * 7. LCS of three Strings
+ * 
+ * 8. Minimum jumps needed to reach end of the array( O(n2) and O(n) approach)
+ * 
+ * 9. Minimum insertions/deletions required to make string palindrome.
+ * 
+ * 10.Maximum length chain of pairs.
+ * 
+ * 11.Building Bridge problem.
+ *  
+ * 12.Maximum Sum Increasing Subsequence.
+ * 
+ * 13.Cutting the rod to obtain maximum value.
+ * 
+ * 14.Maximum Sum Sub array problem(DP and Kadane's algorithm)
+ * 
+ * 15.Maximum sum subsequence so that no two elements are adjacent
+ *  https://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/
+ *  
+ * 16.Maximum sum subsequence where no three are consecutive
+ * https://www.geeksforgeeks.org/maximum-subsequence-sum-such-that-no-three-are-consecutive/
+ * 
+ * 17.Longest subsequence such that difference between adjacents is one
+ * https://www.geeksforgeeks.org/longest-subsequence-such-that-difference-between-adjacents-is-one/
+ * 
+ * 18. Maximum length subsequence with difference between adjacent elements as either 0 or 1
+ * https://www.geeksforgeeks.org/maximum-length-subsequence-difference-adjacent-elements-either-0-1/
+ * 
+ * 19. Coin change problem.
+ * 
+ * 20. Minimum number of coins needed for making a sum.
+ * 
+ * 21. Edit Distance between two strings.
+ * 
+ * 22. Subset Sum Problem
+ * 
+ * 23. 
  * 
  * 
  * 
