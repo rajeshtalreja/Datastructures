5bf9948e20285772b09070bcd6a148808a9a1884
iswitch <iswitch@localhost>
2025-03-29 16:45:18 +0530

    BFS and DFS

---
+++ Graph/src/com/graph/traversal/BFS.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.graph.traversal;
+
+/**
+ * @author iswitch
+ *
+ */
+public class BFS {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Graph/src/com/graph/traversal/DFS.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.graph.traversal;
+
+/**
+ * @author Rajesh Talreja
+ *
+ */
+public class DFS {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
